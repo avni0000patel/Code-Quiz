783f08175d938d0863482a63f70369253cd0dd93
Peanut Butter <avni0000patel@gmail.com>
2022-06-21 14:14:35 -0400

    startBtn

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,26 +1,30 @@
 var timerEl = document.querySelector(".timer");
 
+var startBtn = document.querySelector(".start");
+
 var secondsLeft = 60;
 
-function setTime() {
-    // Sets interval in variable
-    var timerInterval = setInterval(function() {
-        secondsLeft--; // Decrement by 1
-        timerEl.textContent = "Time: " +secondsLeft + " seconds";
-
-
-        if(secondsLeft === 0) {
-            // Stops execution of action at set interval
-            clearInterval(timerInterval);
-            // Calls function to create and append message
-            sendMessage();
+startBtn.addEventListener("click", function() {
+    function setTime() {
+        // Sets interval in variable
+        var timerInterval = setInterval(function() {
+            secondsLeft--; // Decrement by 1
+            timerEl.textContent = "Time: " +secondsLeft + " seconds";
+    
+    
+            if(secondsLeft === 0) {
+                // Stops execution of action at set interval
+                clearInterval(timerInterval);
+                // Calls function to create and append message
+                sendMessage();
+            }
+    
+        }, 1000);
+    
+        function sendMessage() {
+            timerEl.textContent = "Time is up!";
         }
-
-    }, 1000);
-
-    function sendMessage() {
-        timerEl.textContent = "Time is up!";
     }
-}
-
-setTime();
+    
+    setTime();
+});
