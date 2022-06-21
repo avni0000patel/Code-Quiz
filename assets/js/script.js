300e993b00feeaa4d85bea3f1fb9c61c94271268
Peanut Butter <avni0000patel@gmail.com>
2022-06-21 15:32:57 -0400

    container

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,30 +2,31 @@ var timerEl = document.querySelector(".timer");
 
 var startBtn = document.querySelector(".start");
 
+var container = document.querySelector(".container");
+
 var secondsLeft = 60;
 
-startBtn.addEventListener("click", function() {
-    function setTime() {
-        // Sets interval in variable
-        var timerInterval = setInterval(function() {
-            secondsLeft--; // Decrement by 1
-            timerEl.textContent = "Time: " +secondsLeft + " seconds";
+function startQuiz() {
+    container.style.display = "none";
+
+    // Sets interval in variable
+    var timerInterval = setInterval(function() {
+        secondsLeft--; // Decrement by 1
+        timerEl.textContent = "Time: " +secondsLeft + " seconds";
     
-    
-            if(secondsLeft === 0) {
-                // Stops execution of action at set interval
-                clearInterval(timerInterval);
-                // Calls function to create and append message
-                sendMessage();
-            }
-    
-        }, 1000);
-    
-        function sendMessage() {
-            timerEl.textContent = "Time is up!";
+        if(secondsLeft === 0) {
+            // Stops execution of action at set interval
+            clearInterval(timerInterval);
+            // Calls function to create and append message
+            sendMessage();
         }
-    }
     
-    setTime();
- 
-});
+    }, 1000);
+    
+    function sendMessage() {
+        timerEl.textContent = "Time is up!";
+    }
+
+}
+
+startBtn.addEventListener("click", startQuiz);
