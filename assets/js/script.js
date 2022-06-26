65cba1f0e9d839b151b6a50f424629d5686bdc10
Peanut Butter <avni0000patel@gmail.com>
2022-06-26 14:24:57 -0400

    if(questionIndex < myQuestions.length)

--- assets/js/script.js
+++ assets/js/script.js
@@ -74,46 +74,6 @@ var myQuestions = [
             d: "while loop"
         },
         correctAnswer: "c"
-    },
-    {
-        question: "From the given array which index is the letter 'b' on? ['a', 'b', 'c', 'd']",
-        answers: {
-            a: "2",
-            b: "3",
-            c: "1",
-            d: "0"
-        },
-        correctAnswer: "c"
-    },
-    {
-        question: "How do we stop a loop from from repeating indefinitely?",
-        answers: {
-            a: "We have to explicitly end the loop with the break keyword.",
-            b: "When we have iterated through half of the condition.",
-            c: "A loop will stop executing when the condition is true.",
-            d: "A loop will stop executing when the condition is false."
-        },
-        correctAnswer: "d"
-    },
-    {
-        question: "Which statement below is not true about functions in JavaScript?",
-        answers: {
-            a: "Functions are able to be recursive.",
-            b: "A function must always be assigned an identifier",
-            c: "Functions can be reused throughout your code",
-            d: "Functions can receive arguments that can alter the output of a function"
-        },
-        correctAnswer: "b"
-    },
-    {
-        question: "What are the two types of scope JavaScript uses?",
-        answers: {
-            a: "Outside and Inside",
-            b: "Abroad and Local",
-            c: "Surrounding and Inner",
-            d: "Global and Local"
-        },
-        correctAnswer: "d"
     }
 ];
 
@@ -141,11 +101,12 @@ function timer() {
         secondsLeft--; // Decrement by 1
         timerEl.textContent = "Time: " + secondsLeft + " seconds";
 
-        if (secondsLeft <= 0 || questionIndex > myQuestions.length) {
+        if (secondsLeft <= 0 || questionIndex >= myQuestions.length) {
             // Stops execution of action at set interval
             clearInterval(timerInterval);
             // Calls function to create and append message
             sendMessage();
+            quiz.style.display = "none";
         }
 
     }, 1000);
@@ -158,18 +119,17 @@ function sendMessage() {
 function showQuestion() {
     var chosenQuestion = myQuestions[questionIndex];
 
-    console.log(chosenQuestion);
-    console.log(chosenQuestion.question);
-    console.log(chosenQuestion.answers);
     console.log(questionIndex);
     console.log(myQuestions.length);
 
     if(questionIndex < myQuestions.length) {
         questionEl.textContent = chosenQuestion.question;
+        console.log(chosenQuestion.question);
         buttonA.textContent = "a: " + chosenQuestion.answers.a;
         buttonB.textContent = "b: " + chosenQuestion.answers.b;
         buttonC.textContent = "c: " + chosenQuestion.answers.c;
         buttonD.textContent = "d: " + chosenQuestion.answers.d;
+        console.log(chosenQuestion.answers);
     }
 }
 
@@ -178,11 +138,12 @@ function buttonOn(event) {
 
     console.log(event.target.id);
     correct = myQuestions[questionIndex].correctAnswer; 
+    console.log(correct);
+
     userChoiceA = chosenQuestion.answers.a;
     userChoiceB = chosenQuestion.answers.b;
     userChoiceC = chosenQuestion.answers.c;
     userChoiceD = chosenQuestion.answers.d;
-    console.log(correct);
     // if the user clicked button and correct answer is that button, return that button is correct
     if (event.target.id === "a" && correct === "a") {
         console.log(userChoiceA);
