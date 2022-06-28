2ec6eb6ee638da36e0b8441863915408fa2e36d1
Peanut Butter <avni0000patel@gmail.com>
2022-06-28 18:15:44 -0400

    return

--- assets/js/script.js
+++ assets/js/script.js
@@ -221,9 +221,12 @@ function addScore(event) {
     console.log(scoreList);
 
     // Sort from highest to lowest score
-    scoreList = scoreList.sort(function(a, b) {
-        if(a > b) return -1;
-        if(a < b) return 1;
+    scoreList = scoreList.sort(function (a, b) {
+        if(a > b) {
+            return -1;
+        } else if(a < b) {
+            return 1;
+        }
     });
 
     console.log(scoreList);
